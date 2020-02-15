9f3c3be1523ac407ca8bdf296cff9159defc03e3
Daniela Salermi <salermi22@gmail.com>
2020-02-15 20:52:30 -0300

    Fix service path

--- src/app/heroes/detail/detail.component.ts
+++ src/app/heroes/detail/detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { HeroService } from "src/app/services/hero.service";
+import { HeroService } from "../../services/hero.service";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { HeroService } from "src/app/services/hero.service";
+import { HeroService } from "../../services/hero.service";
 import { FormControl } from "@angular/forms";
 
 @Component({
